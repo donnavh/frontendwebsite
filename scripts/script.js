629483239b528d317c8ef9bc07e1c16c9ae085fa
donnavh <118171372+donnavh@users.noreply.github.com>
2022-12-01 11:43:47 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
 console.log("hi");
+
+let deMenuButton = document.querySelector("header > button");
+let deSluitButton = document.querySelector("header nav button");
+
+deMenuButton.addEventListener("click", togglemenu);
+deSluitButton.addEventListener("click", togglemenu);
+
+function togglemenu(){
+    let deNav = document.querySelector("header nav");
+    deNav.classList.toggle("open");
+  }
+  
+  
